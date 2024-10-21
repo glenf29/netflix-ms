dbe29eff627fbb057bea87141a2e265771d8b489
glenf29 <glen.farnandis@gmail.com>
2024-10-21 12:10:02 +0530

    new

--- src/main/java/com/example/netflix_ms/hindimovies.java
+++ src/main/java/com/example/netflix_ms/hindimovies.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class hindimovies {
     @GetMapping("/hindimovies")
     public String getData() {
-        return "Please book Hindimovies From Cloudhaze we are offering more exciting offers , we will provide you 60% discount on this";
+        return "Please book Hindimovies From Cloudhaze and songs we are offering more exciting offers , we will provide you 60% discount on this";
     }
 }
 
